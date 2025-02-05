f80230deb96e3c007200c057612647291ad5c00d
PJ <joppy007@gmail.com>
2025-02-05 13:26:13 -0500

    add index.js and .env and .gitignore and link to mongoDB

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json("Hello mate!");
+  });
+  
+  app.get("*", (req, res) => {
+    res.sendStatus("404");
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+  });
+  
